6d6a653c6d15b53389a8d9ef933d22f4d0c24ccf
Thái <hoangthai.txt@gmail.com>
2023-06-14 22:58:08 +0700

    add cors

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var order = require('./routes/order');
 require("dotenv").config();
+var cors = require('cors')
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -19,6 +19,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/order', order);
