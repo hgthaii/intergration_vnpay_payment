ac25f1287a11633074b4c10202611f8dd04a5622
Thái <hoangthai.txt@gmail.com>
2023-06-15 11:34:38 +0700

    setamount

--- routes/order.js
+++ routes/order.js
@@ -51,7 +51,7 @@ router.post('/create_payment_url', function (req, res, next) {
     let vnpUrl = config.get('vnp_Url');
     let returnUrl = config.get('vnp_ReturnUrl');
     let orderId = moment(date).format('DDHHmmss');
-    let amount = req.body.amount;
+    let amount = 30000
     let bankCode = req.body.bankCode;
     
     let locale = req.body.language;
